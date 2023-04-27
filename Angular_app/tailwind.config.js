b62fb460a187e1e96536c1f2bfa48ff488c6bbba
FZH2001 <97360684+FZH2001@users.noreply.github.com>
2023-04-27 14:14:20 +0000

    Update tailwind.config.js
    Colors added

--- Angular_app/tailwind.config.js
+++ Angular_app/tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./src/**/*.{html,ts}",
 
+    ],
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            'white': '#ffffff',
+            'purple': '#3f3cbb',
+            'midnight': '#121063',
+            'metal': '#565584',
+            'tahiti': '#3ab7bf',
+            'silver': '#ecebff',
+            'bubble-gum': '#ff77e9',
+            'bermuda': '#78dcca',
+            'cream': '#FBF7F2',
+            'shiny-green': '#52E782',
+            'gray': '#C7C6C2'
+        },
+    },
+
+    plugins: [],
+}
