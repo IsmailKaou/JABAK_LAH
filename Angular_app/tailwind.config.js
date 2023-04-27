198fc2db176aa857233c58ffcef7ccad32ee819c
FZH2001 <97360684+FZH2001@users.noreply.github.com>
2023-04-27 14:53:23 +0000

    Colors added

--- Angular_app/tailwind.config.js
+++ Angular_app/tailwind.config.js
@@ -1,26 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
+    content: [
+        "./src/**/*.{html,ts}",
 
-  ],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-      'cream': '#FBF7F2',
-      'shiny-green': '#52E782',
-      'gray': '#C7C6C2'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'white': '#ffffff',
+                'purple': '#3f3cbb',
+                'midnight': '#121063',
+                'metal': '#565584',
+                'tahiti': '#3ab7bf',
+                'silver': '#ecebff',
+                'bubble-gum': '#ff77e9',
+                'bermuda': '#78dcca',
+                'cream': '#FBF7F2',
+                'shiny-green': '#52E782',
+                'gray': '#C7C6C2'
+            }
+        },
     },
-  },
-
-  plugins: [],
+    plugins: [],
 }
