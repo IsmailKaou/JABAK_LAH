2be2a4c0138e7c63c2b8998b38e5b99f10531ad6
FZH2001 <97360684+FZH2001@users.noreply.github.com>
2023-05-03 12:24:25 +0100

    Add files via upload
    Agent dashboard

---
+++ Angular_app/src/app/agent-statistics/agent-statistics.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agent-statistics',
+  templateUrl: './agent-statistics.component.html',
+  styleUrls: ['./agent-statistics.component.css']
+})
+export class AgentStatisticsComponent {
+
+}
